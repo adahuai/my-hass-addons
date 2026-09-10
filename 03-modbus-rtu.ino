4e047727f31a60f7ab19009cc3dd60fa39e720dc
adahuai <adahuai.vn@gmail.com>
2026-09-10 13:22:58 +0700

    Add files via upload

---
+++ 03-modbus-rtu.ino
@@ -0,0 +1,234 @@
+/* *******************************************************************
+   Modbus RTU functions
+
+   sendSerial()
+   - sends Modbus RTU requests to HW serial port (RS485 interface)
+
+   recvSerial()
+   - receives Modbus RTU replies
+   - adjusts headers and forward messages as Modbus TCP/UDP or Modbus RTU over TCP/UDP
+   - sends Modbus TCP/UDP error messages in case Modbus RTU response timeouts
+
+   checkCRC()
+   - checks an array and returns true if CRC is OK
+
+   calculateCRC()
+
+   ***************************************************************** */
+
+
+void sendSerial() {
+  if (!sendMicroTimer.isOver()) {
+    return;
+  }
+  if (queueHeaders.isEmpty()) {
+    return;
+  }
+  static uint8_t txNdx = 0;
+  header_t myHeader = queueHeaders.first();
+  switch (serialState) {
+    case 0:  // IDLE: Optimize queue (prioritize requests from responding slaves) and trigger sending via serial
+      while (priorityReqInQueue && (queueHeaders.first().requestType & PRIORITY_REQUEST) == false) {
+        // move requests to non responding slaves to the tail of the queue
+        for (byte i = 0; i < queueHeaders.first().msgLen; i++) {
+          queueData.push(queueData.shift());
+        }
+        queueHeaders.push(queueHeaders.shift());
+      }
+      serialState++;
+      break;
+    case 1:  // SENDING:
+      {
+        if (mySerial.availableForWrite() > 0 && txNdx == 0) {
+#ifdef RS485_CONTROL_PIN
+          digitalWrite(RS485_CONTROL_PIN, RS485_TRANSMIT);  // Enable RS485 Transmit
+#endif                                                      /* RS485_CONTROL_PIN */
+          crc = 0xFFFF;
+        }
+        while (mySerial.availableForWrite() > 0 && txNdx < myHeader.msgLen) {
+          mySerial.write(queueData[txNdx]);
+          calculateCRC(queueData[txNdx]);
+          txNdx++;
+        }
+        if (mySerial.availableForWrite() > 1 && txNdx == myHeader.msgLen) {
+          mySerial.write(lowByte(crc));  // send CRC, low byte first
+          mySerial.write(highByte(crc));
+          txNdx++;
+        }
+        if (mySerial.availableForWrite() == SERIAL_TX_BUFFER_SIZE - 1 && txNdx > myHeader.msgLen) {
+          // wait for last byte (incl. CRC) to be sent from serial Tx buffer
+          // this if statement is not very reliable (too fast)
+          // Serial.isFlushed() method is needed....see https://github.com/arduino/Arduino/pull/3737
+          txNdx = 0;
+          mySerial.flush();
+#ifdef RS485_CONTROL_PIN
+          // sendMicroTimer.sleep(frameDelay());  // Short delay before we toggle the RS485_CONTROL_PIN and disable RS485 transmit. Not needed if we use flush()
+#endif /* RS485_CONTROL_PIN */
+          serialState++;
+        }
+      }
+      break;
+    case 2:  // DELAY:
+      {
+#ifdef ENABLE_EXTENDED_WEBUI
+        data.rtuCnt[DATA_TX] += myHeader.msgLen;
+        data.rtuCnt[DATA_TX] += 2;
+#endif
+#ifdef RS485_CONTROL_PIN
+        digitalWrite(RS485_CONTROL_PIN, RS485_RECEIVE);  // Disable RS485 Transmit
+#endif                                                   /* RS485_CONTROL_PIN */
+        myHeader.atts++;
+        queueHeaders.shift();
+        queueHeaders.unshift(myHeader);
+        uint32_t delay = data.config.serialTimeout;
+        if (myHeader.requestType & SCAN_REQUEST) delay = SCAN_TIMEOUT;  // fixed timeout for scan requests
+        sendMicroTimer.sleep(delay * 1000UL);
+        serialState++;
+      }
+      break;
+    case 3:  // WAITING: Deal with Serial timeouts (i.e. Modbus RTU timeouts)
+      {
+        if (myHeader.requestType & SCAN_REQUEST) {  // Only one attempt for scan request (we do not count attempts)
+          deleteRequest();
+        } else if (myHeader.atts >= data.config.serialAttempts) {
+          // send modbus error 0x0B (Gateway Target Device Failed to Respond) - usually means that target device (address) is not present
+          setSlaveStatus(queueData[0], SLAVE_ERROR_0B, true, false);
+          byte MBAP[] = { myHeader.tid[0],
+                          myHeader.tid[1],
+                          0x00,
+                          0x00,
+                          0x00,
+                          0x03 };
+          byte PDU[5] = { queueData[0],
+                          byte(queueData[1] + 0x80),
+                          0x0B };
+          crc = 0xFFFF;
+          for (byte i = 0; i < 3; i++) {
+            calculateCRC(PDU[i]);
+          }
+          PDU[3] = lowByte(crc);  // send CRC, low byte first
+          PDU[4] = highByte(crc);
+          sendResponse(MBAP, PDU, 5);
+          data.errorCnt[ERROR_TIMEOUT]++;
+        } else {
+          setSlaveStatus(queueData[0], SLAVE_ERROR_0B_QUEUE, true, false);
+          data.errorCnt[ERROR_TIMEOUT]++;
+        }                 // if (myHeader.atts >= MAX_RETRY)
+        serialState = 0;  // IDLE
+      }
+      break;
+    default:
+      break;
+  }
+}
+
+void recvSerial() {
+  static uint8_t rxNdx = 0;
+  static byte serialIn[MODBUS_SIZE];
+  while (mySerial.available() > 0) {
+    byte b = mySerial.read();
+    if (rxNdx == 0 && queueData[0] != b) { // TODO maybe duplicate check? See the check bellow.
+      if (recvMicroTimer.isOver())
+        break;
+      // wait a bit to the buffer to clean up
+      recvMicroTimer.sleep(charTimeOut());
+      data.errorCnt[ERROR_RTU]++;
+    } else if (rxNdx < MODBUS_SIZE) {
+      serialIn[rxNdx] = b;
+      rxNdx++;
+    }  // if frame longer than maximum allowed, CRC will fail and data.errorCnt[ERROR_RTU] will be recorded down the road
+    recvMicroTimer.sleep(charTimeOut());
+    sendMicroTimer.sleep(data.config.frameDelay * 1000UL);  // delay next serial write
+  }
+  if (recvMicroTimer.isOver() && rxNdx != 0) {
+    // Process Serial data
+    // Checks: 1) CRC; 2) address of incoming packet against first request in queue; 3) only expected responses are forwarded to TCP/UDP
+    header_t myHeader = queueHeaders.first();
+    if (checkCRC(serialIn, rxNdx) == true && serialIn[0] == queueData[0] && serialState == WAITING) {
+      if (serialIn[1] > 0x80 && (myHeader.requestType & SCAN_REQUEST) == false) {
+        setSlaveStatus(serialIn[0], SLAVE_ERROR_0X, true, false);
+      } else {
+        setSlaveStatus(serialIn[0], SLAVE_OK, true, myHeader.requestType & SCAN_REQUEST);
+      }
+      byte MBAP[] = {
+        myHeader.tid[0],
+        myHeader.tid[1],
+        0x00,
+        0x00,
+        highByte(rxNdx - 2),
+        lowByte(rxNdx - 2)
+      };
+      sendResponse(MBAP, serialIn, rxNdx);
+      serialState = IDLE;
+    } else {
+      data.errorCnt[ERROR_RTU]++;
+    }
+#ifdef ENABLE_EXTENDED_WEBUI
+    data.rtuCnt[DATA_RX] += rxNdx;
+#endif /* ENABLE_EXTENDED_WEBUI */
+    rxNdx = 0;
+  }
+}
+
+void sendResponse(const byte MBAP[], const byte PDU[], const uint16_t pduLength) {
+  header_t myHeader = queueHeaders.first();
+  responseLen = 0;
+  while (responseLen < pduLength) {  // include CRC
+    if (responseLen < MAX_RESPONSE_LEN) {
+      response[responseLen] = PDU[responseLen];
+    }
+    responseLen++;
+  }
+  if (myHeader.requestType & UDP_REQUEST) {
+    Udp.beginPacket(myHeader.remIP, myHeader.remPort);
+    if (data.config.enableRtuOverTcp) Udp.write(PDU, pduLength);
+    else {
+      Udp.write(MBAP, 6);
+      Udp.write(PDU, pduLength - 2);  //send without CRC
+    }
+    Udp.endPacket();
+#ifdef ENABLE_EXTENDED_WEBUI
+    data.ethCnt[DATA_TX] += pduLength;
+    if (!data.config.enableRtuOverTcp) data.ethCnt[DATA_TX] += 4;
+#endif /* ENABLE_EXTENDED_WEBUI */
+  } else if (myHeader.requestType & TCP_REQUEST) {
+    byte sock = myHeader.requestType & TCP_REQUEST_MASK;
+    EthernetClient client = EthernetClient(sock);
+    if (W5100.readSnSR(sock) == SnSR::ESTABLISHED && W5100.readSnDPORT(sock) == myHeader.remPort) {  // Check remote port should be enough or check also rem IP?
+      if (data.config.enableRtuOverTcp) client.write(PDU, pduLength);
+      else {
+        client.write(MBAP, 6);
+        client.write(PDU, pduLength - 2);  //send without CRC
+      }
+#ifdef ENABLE_EXTENDED_WEBUI
+      data.ethCnt[DATA_TX] += pduLength;
+      if (!data.config.enableRtuOverTcp) data.ethCnt[DATA_TX] += 4;
+#endif /* ENABLE_EXTENDED_WEBUI */
+    }  // TODO TCP Connection Error
+  }    // else SCAN_REQUEST (no data.ethCnt[DATA_TX], but yes delete request)
+  deleteRequest();
+}
+
+bool checkCRC(byte buf[], int16_t len) {
+  crc = 0xFFFF;
+  for (byte i = 0; i < len - 2; i++) {
+    calculateCRC(buf[i]);
+  }
+  if (highByte(crc) == buf[len - 1] && lowByte(crc) == buf[len - 2]) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void calculateCRC(byte b) {
+  crc ^= (uint16_t)b;              // XOR byte into least sig. byte of crc
+  for (byte i = 8; i != 0; i--) {  // Loop over each bit
+    if ((crc & 0x0001) != 0) {     // If the LSB is set
+      crc >>= 1;                   // Shift right and XOR 0xA001
+      crc ^= 0xA001;
+    } else        // Else LSB is not set
+      crc >>= 1;  // Just shift right
+  }
+  // Note, this number has low and high bytes swapped, so use it accordingly (or swap bytes)
+}
---
+++ arduino-modbus-rtu-tcp-gateway.ino
@@ -0,0 +1,284 @@
+/* Arduino-based Modbus RTU (slaves) to Modbus TCP/UDP (master) gateway with web interface
+
+  Version history
+  v0.1 2020-04-05 Initial commit
+  v0.2 2021-03-02 Random MAC generation
+  v1.0 2021-03-20 Add web interface, settings stored in EEPROM
+  v2.0 2021-04-01 Improve random MAC algorithm (Marsaglia algorithm from https://github.com/RobTillaart/randomHelpers),
+                  replace some libraries with more efficient code, compatibility with Arduino Mega
+  v2.1 2021-04-12 Code optimisation
+  v2.2 2021-06-06 Fix TCP closed socket, support RS485 modules with hardware automatic flow control
+  v2.3 2021-09-10 Fix IPAddress cast (gateway freeze)
+  v2.4 2021-10-15 Add SW version. Forced factory reset (load defaut settings from sketch) on MAJOR version change.
+  v3.0 2021-11-07 Improve POST parameters processing, bugfix 404 and 204 error headers. 
+  v3.1 2022-01-28 Code optimization, bugfix DHCP settings.
+  v3.2 2022-06-04 Reduce program size (so that it fits on Nano), ethernet data counter only available when ENABLE_EXTENDED_WEBUI.
+  v4.0 2023-01-05 Modbus statistics and error reporting on "Modbus Status" page, add Frame Delay setting for Modbus RTU
+                  Optimize Modbus timeout and attempts, significant reduction of code size
+  v4.1 2023-01-14 Fetch API, bugfix MAX485
+  v5.0 2023-02-19 Send Modbus Request from WebUI, optimized POST parameter processing (less RAM consumption), select baud rate in WebUI,
+                  improved TCP socket management, Modbus TCP Idle Timeout settings
+  v6.0 2023-03-18 Save error counters to EEPROM, code optimization, separate file for advanced settings
+  v6.1 2023-04-12 Code optimization
+  v7.0 2023-07-21 Manual MAC, better data types
+  v7.1 2023-08-25 Simplify EEPROM read and write, Tools page
+  v7.2 2023-10-20 Disable DHCP renewal fallback, better advanced_settings.h layout
+                  ENABLE_EXTENDED_WEBUI and ENABLE_DHCP is set by default for Mega
+  v7.3 2024-01-16 Bugfix Modbus RTU Request form, code comments
+  v7.4 2024-12-16 CSS improvement, code optimization, simplify DHCP renew, better README (solution to ethernet reset issue)
+  v8.0 2025-03-09 Fix 404 error page, code optimization
+  v8.1 2025-03-30 Read away trash coming before slave id
+*/
+
+const byte VERSION[] = { 8, 1 };
+
+#include <SPI.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <utility/w5100.h>
+#include <CircularBuffer.hpp>  // CircularBuffer https://github.com/rlogiacco/CircularBuffer
+#include <EEPROM.h>
+#include <StreamLib.h>  // StreamLib https://github.com/jandrassy/StreamLib
+
+// these are used by CreateTrulyRandomSeed() function
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <util/atomic.h>
+
+#include "advanced_settings.h"
+
+typedef struct {
+  byte ip[4];
+  byte subnet[4];
+  byte gateway[4];
+#ifdef ENABLE_DHCP
+  byte dns[4];      // only used if ENABLE_DHCP
+  bool enableDhcp;  // only used if ENABLE_DHCP
+#endif
+  uint16_t tcpPort;
+  uint16_t udpPort;
+  uint16_t webPort;
+  bool enableRtuOverTcp;
+  uint16_t tcpTimeout;
+  uint16_t baud;
+  byte serialConfig;
+  byte frameDelay;
+  uint16_t serialTimeout;
+  byte serialAttempts;
+} config_t;
+
+const config_t DEFAULT_CONFIG = {
+  DEFAULT_STATIC_IP,
+  DEFAULT_SUBMASK,
+  DEFAULT_GATEWAY,
+#ifdef ENABLE_DHCP
+  DEFAULT_DNS,
+  DEFAULT_AUTO_IP,
+#endif
+  DEFAULT_TCP_PORT,
+  DEFAULT_UDP_PORT,
+  DEFAULT_WEB_PORT,
+  DEFAULT_RTU_OVER_TCP,
+  DEFAULT_TCP_TIMEOUT,
+  DEFAULT_BAUD_RATE,
+  DEFAULT_SERIAL_CONFIG,
+  DEFAULT_FRAME_DELAY,
+  DEFAULT_RESPONSE_TIMEPOUT,
+  DEFAULT_ATTEMPTS,
+};
+
+enum status_t : byte {
+  SLAVE_OK,              // Slave Responded
+  SLAVE_ERROR_0X,        // Slave Responded with Error (Codes 1~8)
+  SLAVE_ERROR_0A,        // Gateway Overloaded (Code 10)
+  SLAVE_ERROR_0B,        // Slave Failed to Respond (Code 11)
+  SLAVE_ERROR_0B_QUEUE,  // Slave Failed to Respond (Code 11) & is in Queue (not shown in web UI)
+  ERROR_TIMEOUT,         // Response Timeout
+  ERROR_RTU,             // Invalid RTU Response
+  ERROR_TCP,             // Invalid TCP/UDP Request
+  ERROR_LAST             // Number of status flags in this enum. Must be the last element within this enum!!
+};
+
+enum flow_t : byte {
+  DATA_TX,
+  DATA_RX,
+  DATA_LAST  // Number of status flags in this enum. Must be the last element within this enum!!
+};
+
+typedef struct {
+  uint32_t eepromWrites;          // Number of EEPROM write cycles (persistent, it is never cleared during factory resets)
+  byte major;                     // major version
+  byte mac[6];                    // MAC Address (initial value is random generated)
+  config_t config;                // configuration values
+  uint32_t errorCnt[ERROR_LAST];  // array for storing error counters
+#ifdef ENABLE_EXTENDED_WEBUI
+  uint32_t rtuCnt[DATA_LAST];  // array for storing RTU data counters
+  uint32_t ethCnt[DATA_LAST];  // array for storing ethernet data counters
+#endif                         /* ENABLE_EXTENDED_WEBUI */
+} data_t;
+
+data_t data;
+
+typedef struct {
+  byte tid[2];       // MBAP Transaction ID
+  byte msgLen;       // lenght of Modbus message stored in queueData
+  IPAddress remIP;   // remote IP for UDP client (UDP response is sent back to remote IP)
+  uint16_t remPort;  // remote port for UDP client (UDP response is sent back to remote port)
+  byte requestType;  // TCP client who sent the request
+  byte atts;         // attempts counter
+} header_t;
+
+// bool arrays for storing Modbus RTU status of individual slaves
+byte slaveStatus[SLAVE_ERROR_0B_QUEUE + 1][(MAX_SLAVES + 1 + 7) / 8];  // SLAVE_ERROR_0B_QUEUE is the last status of slaves
+
+// each request is stored in 3 queues (all queues are written to, read and deleted in sync)
+CircularBuffer<header_t, MAX_QUEUE_REQUESTS> queueHeaders;  // queue of requests' headers and metadata
+CircularBuffer<byte, MAX_QUEUE_DATA> queueData;             // queue of PDU data
+
+
+/****** ETHERNET AND SERIAL ******/
+
+byte maxSockNum = MAX_SOCK_NUM;
+
+#ifdef ENABLE_DHCP
+bool dhcpSuccess = false;
+#endif /* ENABLE_DHCP */
+
+EthernetUDP Udp;
+EthernetServer modbusServer(DEFAULT_CONFIG.tcpPort);
+EthernetServer webServer(DEFAULT_CONFIG.webPort);
+
+/****** TIMERS AND STATE MACHINE ******/
+
+class MicroTimer {
+private:
+  uint32_t timestampLastHitMs;
+  uint32_t sleepTimeMs;
+public:
+  boolean isOver();
+  void sleep(uint32_t sleepTimeMs);
+};
+boolean MicroTimer::isOver() {
+  if ((uint32_t)(micros() - timestampLastHitMs) > sleepTimeMs) {
+    return true;
+  }
+  return false;
+}
+void MicroTimer::sleep(uint32_t sleepTimeMs) {
+  this->sleepTimeMs = sleepTimeMs;
+  timestampLastHitMs = micros();
+}
+
+class Timer {
+private:
+  uint32_t timestampLastHitMs;
+  uint32_t sleepTimeMs;
+public:
+  boolean isOver();
+  void sleep(uint32_t sleepTimeMs);
+};
+boolean Timer::isOver() {
+  if ((uint32_t)(millis() - timestampLastHitMs) > sleepTimeMs) {
+    return true;
+  }
+  return false;
+}
+void Timer::sleep(uint32_t sleepTimeMs) {
+  this->sleepTimeMs = sleepTimeMs;
+  timestampLastHitMs = millis();
+}
+
+MicroTimer recvMicroTimer;
+MicroTimer sendMicroTimer;
+Timer eepromTimer;    // timer to delay writing statistics to EEPROM
+Timer checkEthTimer;  // timer to check SPI connection with ethernet shield
+
+#define RS485_TRANSMIT HIGH
+#define RS485_RECEIVE LOW
+
+byte scanCounter = 1;  // Start Modbus RTU scan after boot
+enum state_t : byte {
+  IDLE,
+  SENDING,
+  DELAY,
+  WAITING
+};
+
+byte serialState;
+
+
+/****** RUN TIME AND DATA COUNTERS ******/
+
+bool scanReqInQueue = false;  // Scan request is in the queue
+byte priorityReqInQueue;      // Counter for priority requests in the queue
+
+byte response[MAX_RESPONSE_LEN];  // buffer to store the last Modbus response
+byte responseLen;                 // stores actual length of the response shown in WebUI
+
+uint16_t queueDataSize;
+byte queueHeadersSize;
+
+#ifdef ENABLE_EXTENDED_WEBUI
+// store uptime seconds (includes seconds counted before millis() overflow)
+uint32_t seconds;
+// store last millis() so that we can detect millis() overflow
+uint32_t last_milliseconds = 0;
+// store seconds passed until the moment of the overflow so that we can add them to "seconds" on the next call
+int32_t remaining_seconds;
+// Data counters (we only use uint32_t in ENABLE_EXTENDED_WEBUI, to save flash memory)
+#endif /* ENABLE_EXTENDED_WEBUI */
+
+volatile uint32_t seed1;  // seed1 is generated by CreateTrulyRandomSeed()
+volatile int8_t nrot;
+uint32_t seed2 = 17111989;  // seed2 is static
+
+
+/****** SETUP: RUNS ONCE ******/
+
+void setup() {
+  CreateTrulyRandomSeed();
+  EEPROM.get(DATA_START, data);
+  // is configuration already stored in EEPROM?
+  if (data.major != VERSION[0]) {
+    data.major = VERSION[0];
+    // load default configuration from flash memory
+    data.config = DEFAULT_CONFIG;
+    generateMac();
+    resetStats();
+    updateEeprom();
+  }
+  startSerial();
+  startEthernet();
+}
+
+/****** LOOP ******/
+
+void loop() {
+
+  scanRequest();
+  sendSerial();
+  recvUdp();
+  recvSerial();
+
+  manageSockets();
+
+  if (EEPROM_INTERVAL > 0 && eepromTimer.isOver() == true) {
+    updateEeprom();
+  }
+
+  // could help with freeze after power loss reported by some users, not enabled yet
+  // if (CHECK_ETH_INTERVAL > 0 && checkEthTimer.isOver() == true) {
+  //   checkEthernet();
+  // }
+
+  if (rollover()) {
+    resetStats();
+    updateEeprom();
+  }
+#ifdef ENABLE_EXTENDED_WEBUI
+  maintainUptime();  // maintain uptime in case of millis() overflow
+#endif               /* ENABLE_EXTENDED_WEBUI */
+#ifdef ENABLE_DHCP
+  Ethernet.maintain();
+#endif /* ENABLE_DHCP */
+}
