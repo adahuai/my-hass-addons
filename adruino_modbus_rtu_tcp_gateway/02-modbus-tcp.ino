6094d2c10a5c4e996c859bbef387014054a6699f
adahuai <adahuai.vn@gmail.com>
2026-09-10 13:34:59 +0700

    Add files via upload

---
+++ adruino_modbus_rtu_tcp_gateway/02-modbus-tcp.ino
@@ -0,0 +1,268 @@
+/* *******************************************************************
+   Modbus TCP/UDP functions
+
+   recvUdp()
+   - receives Modbus UDP (or Modbus RTU over UDP) messages
+   - calls checkRequest
+
+   recvTcp()
+   - receives Modbus TCP (or Modbus RTU over TCP) messages
+   - calls checkRequest
+
+   checkRequest()
+   - checks Modbus TCP/UDP requests (correct MBAP header, CRC in case of Modbus RTU over TCP/UDP)
+   - checks availability of queue
+   - stores requests into queue or returns an error
+
+   scanRequest()
+   - inserts scan request into queue
+
+   deleteRequest()
+   - deletes requests from queue
+
+   clearQueue()
+   - clears queue and corresponding counters
+
+   getSlaveStatus(), setSlaveStatus()
+   - read from and write to bool array
+
+   ***************************************************************** */
+
+byte masks[8] = { 1, 2, 4, 8, 16, 32, 64, 128 };
+
+// Stored in "header.requestType"
+#define PRIORITY_REQUEST B10000000  // Request to slave which is not "nonresponding"
+#define SCAN_REQUEST B01000000      // Request triggered by slave scanner
+#define UDP_REQUEST B00100000       // UDP request
+#define TCP_REQUEST B00001000       // TCP request
+#define TCP_REQUEST_MASK B00000111  // Stores TCP client number
+
+uint16_t crc;
+
+/**************************************************************************/
+/*!
+  @brief Receives Modbus UDP (or Modbus RTU over UDP) messages, calls @ref checkRequest()
+*/
+/**************************************************************************/
+void recvUdp() {
+  uint16_t msgLength = Udp.parsePacket();
+  if (msgLength) {
+#ifdef ENABLE_EXTENDED_WEBUI
+    data.ethCnt[DATA_RX] += msgLength;
+#endif                               /* ENABLE_EXTENDED_WEBUI */
+    byte inBuffer[MODBUS_SIZE + 4];  // Modbus TCP frame is 4 bytes longer than Modbus RTU frame
+                                     // Modbus TCP/UDP frame: [0][1] transaction ID, [2][3] protocol ID, [4][5] length and [6] unit ID (address)..... no CRC
+                                     // Modbus RTU frame: [0] address.....[n-1][n] CRC
+    memset(inBuffer, 0, sizeof(inBuffer));
+    Udp.read(inBuffer, sizeof(inBuffer));
+    while (Udp.available()) Udp.read();
+    byte errorCode = checkRequest(inBuffer, msgLength, (uint32_t)Udp.remoteIP(), Udp.remotePort(), UDP_REQUEST);
+    if (errorCode) {
+      // send back message with error code
+      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+      if (!data.config.enableRtuOverTcp) {
+        Udp.write(inBuffer, 5);
+        Udp.write(0x03);
+      }
+      byte addressPos = 6 * !data.config.enableRtuOverTcp;  // position of slave address in the incoming TCP/UDP message (0 for Modbus RTU over TCP/UDP and 6 for Modbus RTU over TCP/UDP)
+      Udp.write(inBuffer[addressPos]);                      // address
+      Udp.write(inBuffer[addressPos + 1] + 0x80);           // function + 0x80
+      Udp.write(errorCode);
+      if (data.config.enableRtuOverTcp) {
+        crc = 0xFFFF;
+        calculateCRC(inBuffer[addressPos]);
+        calculateCRC(inBuffer[addressPos + 1] + 0x80);
+        calculateCRC(errorCode);
+        Udp.write(lowByte(crc));  // send CRC, low byte first
+        Udp.write(highByte(crc));
+      }
+      Udp.endPacket();
+#ifdef ENABLE_EXTENDED_WEBUI
+      data.ethCnt[DATA_TX] += 5;
+      if (!data.config.enableRtuOverTcp) data.ethCnt[DATA_TX] += 4;
+#endif /* ENABLE_EXTENDED_WEBUI */
+    }
+  }
+}
+
+/**************************************************************************/
+/*!
+  @brief Receives Modbus TCP (or Modbus RTU over TCP) messages, calls @ref checkRequest()
+*/
+/**************************************************************************/
+void recvTcp(EthernetClient &client) {
+  uint16_t msgLength = client.available();
+#ifdef ENABLE_EXTENDED_WEBUI
+  data.ethCnt[DATA_RX] += msgLength;
+#endif                             /* ENABLE_EXTENDED_WEBUI */
+  byte inBuffer[MODBUS_SIZE + 4];  // Modbus TCP frame is 4 bytes longer than Modbus RTU frame
+                                   // Modbus TCP/UDP frame: [0][1] transaction ID, [2][3] protocol ID, [4][5] length and [6] unit ID (address).....
+                                   // Modbus RTU frame: [0] address.....
+  memset(inBuffer, 0, sizeof(inBuffer));
+  client.read(inBuffer, sizeof(inBuffer));
+  while (client.available()) client.read();
+  byte errorCode = checkRequest(inBuffer, msgLength, {}, client.remotePort(), TCP_REQUEST | client.getSocketNumber());
+  if (errorCode) {
+    // send back message with error code
+    byte i = 0;
+    byte outBuffer[9];
+    if (!data.config.enableRtuOverTcp) {
+      memcpy(outBuffer, inBuffer, 5);
+      outBuffer[5] = 0x03;
+      i = 6;
+    }
+    byte addressPos = 6 * !data.config.enableRtuOverTcp;  // position of slave address in the incoming TCP/UDP message (0 for Modbus RTU over TCP/UDP and 6 for Modbus RTU over TCP/UDP)
+    outBuffer[i++] = inBuffer[addressPos];                // address
+    outBuffer[i++] = inBuffer[addressPos + 1] + 0x80;     // function + 0x80
+    outBuffer[i++] = errorCode;
+    if (data.config.enableRtuOverTcp) {
+      crc = 0xFFFF;
+      calculateCRC(inBuffer[addressPos]);
+      calculateCRC(inBuffer[addressPos + 1] + 0x80);
+      calculateCRC(errorCode);
+      outBuffer[i++] = lowByte(crc);  // send CRC, low byte first
+      outBuffer[i++] = highByte(crc);
+    }
+    client.write(outBuffer, i);
+#ifdef ENABLE_EXTENDED_WEBUI
+    data.ethCnt[DATA_TX] += 5;
+    if (!data.config.enableRtuOverTcp) data.ethCnt[DATA_TX] += 4;
+#endif /* ENABLE_EXTENDED_WEBUI */
+  }
+}
+
+
+void scanRequest() {
+  // Insert scan request into queue, allow only one scan request in a queue
+  static byte scanCommand[] = { SCAN_FUNCTION_FIRST, 0x00, SCAN_DATA_ADDRESS, 0x00, 0x01 };
+  if (scanCounter != 0 && queueHeaders.available() > 1 && queueData.available() > sizeof(scanCommand) + 1 && scanReqInQueue == false) {
+    scanReqInQueue = true;
+    // Store scan request in request queue
+    queueHeaders.push(header_t{
+      { 0x00, 0x00 },           // tid[2]
+      sizeof(scanCommand) + 1,  // msgLen
+      { 0, 0, 0, 0 },           // remIP
+      0,                        // remPort
+      SCAN_REQUEST,             // requestType
+      0,                        // atts
+    });
+    queueData.push(scanCounter);  // address of the scanned slave
+    for (byte i = 0; i < sizeof(scanCommand); i++) {
+      queueData.push(scanCommand[i]);
+    }
+    if (scanCommand[0] == SCAN_FUNCTION_FIRST) {
+      scanCommand[0] = SCAN_FUNCTION_SECOND;
+    } else {
+      scanCommand[0] = SCAN_FUNCTION_FIRST;
+      scanCounter++;
+    }
+    if (scanCounter == MAX_SLAVES + 1) scanCounter = 0;
+  }
+}
+
+/**************************************************************************/
+/*!
+  @brief Checks Modbus TCP/UDP requests (correct MBAP header,
+  CRC in case of Modbus RTU over TCP/UDP), checks availability of queue,
+  stores requests into queue or returns an error.
+  @param inBuffer Modbus TCP/UDP requests
+  @param msgLength Length of the Modbus TCP/UDP requests
+  @param remoteIP Remote IP
+  @param remotePort Remote port
+  @param requestType UDP or TCP, priority or scan request
+  @return Modbus error code to be sent back to the recipient.
+*/
+/**************************************************************************/
+byte checkRequest(byte inBuffer[], uint16_t msgLength, const uint32_t remoteIP, const uint16_t remotePort, byte requestType) {
+  byte addressPos = 6 * !data.config.enableRtuOverTcp;  // position of slave address in the incoming TCP/UDP message (0 for Modbus RTU over TCP/UDP and 6 for Modbus RTU over TCP/UDP)
+  if (data.config.enableRtuOverTcp) {                   // check CRC for Modbus RTU over TCP/UDP
+    if (checkCRC(inBuffer, msgLength) == false) {
+      data.errorCnt[ERROR_TCP]++;
+      return 0;  // drop request and do not return any error code
+    }
+  } else {  // check MBAP header structure for Modbus TCP/UDP
+    if (inBuffer[2] != 0x00 || inBuffer[3] != 0x00 || inBuffer[4] != 0x00 || inBuffer[5] != msgLength - 6) {
+      data.errorCnt[ERROR_TCP]++;
+      return 0;  // drop request and do not return any error code
+    }
+  }
+  msgLength = msgLength - addressPos - (2 * data.config.enableRtuOverTcp);  // in Modbus RTU over TCP/UDP do not store CRC
+  // check if we have space in request queue
+  if (queueHeaders.available() < 1 || queueData.available() < msgLength) {
+    setSlaveStatus(inBuffer[addressPos], SLAVE_ERROR_0A, true, false);
+    return 0x0A;  // return Modbus error code 10 (Gateway Overloaded)
+  }
+  // allow only one request to non responding slaves
+  if (getSlaveStatus(inBuffer[addressPos], SLAVE_ERROR_0B_QUEUE)) {
+    data.errorCnt[SLAVE_ERROR_0B]++;
+    return 0x0B;  // return Modbus error code 11 (Gateway Target Device Failed to Respond)
+  } else if (getSlaveStatus(inBuffer[addressPos], SLAVE_ERROR_0B)) {
+    setSlaveStatus(inBuffer[addressPos], SLAVE_ERROR_0B_QUEUE, true, false);
+  } else {
+    // Add PRIORITY_REQUEST flag to requests for responding slaves
+    requestType = requestType | PRIORITY_REQUEST;
+    priorityReqInQueue++;
+  }
+  if (inBuffer[addressPos] == 0x00) {          // Modbus Broadcast
+    requestType = requestType | SCAN_REQUEST;  // Treat broadcast as scan (only one attempt, short timeout, do not expect response)
+  }
+  // all checkes passed OK, we can store the incoming data in request queue
+  if (requestType & TCP_REQUEST) {
+    socketInQueue[requestType & TCP_REQUEST_MASK]++;
+  }
+  // Store in request queue
+  queueHeaders.push(header_t{
+    { inBuffer[0], inBuffer[1] },  // tid[2] (ignored in Modbus RTU over TCP/UDP)
+    byte(msgLength),               // msgLen
+    (IPAddress)remoteIP,           // remIP
+    (unsigned int)remotePort,      // remPort
+    byte(requestType),             // requestType
+    0,                             // atts
+  });
+  for (byte i = 0; i < msgLength; i++) {
+    queueData.push(inBuffer[i + addressPos]);
+  }
+  if (queueData.size() > queueDataSize) queueDataSize = queueData.size();
+  if (queueHeaders.size() > queueHeadersSize) queueHeadersSize = queueHeaders.size();
+  return 0;
+}
+
+void deleteRequest()  // delete request from queue
+{
+  header_t myHeader = queueHeaders.first();
+  if (myHeader.requestType & SCAN_REQUEST) scanReqInQueue = false;
+  if (myHeader.requestType & TCP_REQUEST) socketInQueue[myHeader.requestType & TCP_REQUEST_MASK]--;
+  if (myHeader.requestType & PRIORITY_REQUEST) priorityReqInQueue--;
+  for (byte i = 0; i < myHeader.msgLen; i++) {
+    queueData.shift();
+  }
+  queueHeaders.shift();
+}
+
+void clearQueue() {
+  queueHeaders.clear();
+  queueData.clear();
+  scanReqInQueue = false;
+  priorityReqInQueue = false;
+  memset(socketInQueue, 0, sizeof(socketInQueue));
+  memset(slaveStatus[SLAVE_ERROR_0B_QUEUE], 0, sizeof(slaveStatus[SLAVE_ERROR_0B_QUEUE]));
+  sendMicroTimer.sleep(0);
+}
+
+bool getSlaveStatus(const byte slave, const byte status) {
+  if (slave >= MAX_SLAVES) return false;  // error
+  return (slaveStatus[status][slave / 8] & masks[slave & 7]) > 0;
+}
+
+void setSlaveStatus(const byte slave, byte status, const bool value, const bool isScan) {
+  if (slave >= MAX_SLAVES || status > SLAVE_ERROR_0B_QUEUE) return;  // error
+  if (value == 0) {
+    slaveStatus[status][slave / 8] &= ~masks[slave & 7];
+  } else {
+    for (byte i = 0; i <= SLAVE_ERROR_0B_QUEUE; i++) {
+      slaveStatus[i][slave / 8] &= ~masks[slave & 7];  // set all other flags to false, SLAVE_ERROR_0B_QUEUE is the last slave status
+    }
+    slaveStatus[status][slave / 8] |= masks[slave & 7];
+    if (status != SLAVE_ERROR_0B_QUEUE && isScan == false) data.errorCnt[status]++;  // there is no counter for SLAVE_ERROR_0B_QUEUE, ignor scans in statistics
+  }
+}
---
+++ adruino_modbus_rtu_tcp_gateway/04-webserver.ino
@@ -0,0 +1,429 @@
+const byte URI_SIZE = 24;   // a smaller buffer for uri
+const byte POST_SIZE = 24;  // a smaller buffer for single post parameter + key
+
+// Actions that need to be taken after saving configuration.
+enum action_type : byte {
+  ACT_NONE,
+  ACT_DEFAULT,        // Load default factory settings (but keep MAC address)
+  ACT_MAC,            // Generate new random MAC
+  ACT_REBOOT,         // Reboot the microcontroller
+  ACT_RESET_ETH,      // Ethernet reset
+  ACT_RESET_SERIAL,   // Serial reset
+  ACT_SCAN,           // Initialize RTU scan
+  ACT_RESET_STATS,    // Reset Modbus Statistics
+  ACT_CLEAR_REQUEST,  // Clear Modbus Request form
+  ACT_WEB             // Restart webserver
+};
+enum action_type action;
+
+// Pages served by the webserver. Order of elements defines the order in the left menu of the web UI.
+// URL of the page (*.htm) contains number corresponding to its position in this array.
+// The following enum array can have a maximum of 10 elements (incl. PAGE_NONE and PAGE_WAIT)
+enum page : byte {
+  PAGE_ERROR,  // 404 Error
+  PAGE_INFO,
+  PAGE_STATUS,
+  PAGE_IP,
+  PAGE_TCP,
+  PAGE_RTU,
+  PAGE_TOOLS,
+  PAGE_WAIT,  // page with "Reloading. Please wait..." message.
+  PAGE_DATA,  // d.json
+};
+
+// Keys for POST parameters, used in web forms and processed by processPost() function.
+// Using enum ensures unique identification of each POST parameter key and consistence across functions.
+// In HTML code, each element will apear as number corresponding to its position in this array.
+enum post_key : byte {
+  POST_NONE,  // reserved for NULL
+  POST_DHCP,  // enable DHCP
+  POST_MAC,
+  POST_MAC_1,
+  POST_MAC_2,
+  POST_MAC_3,
+  POST_MAC_4,
+  POST_MAC_5,
+  POST_IP,
+  POST_IP_1,
+  POST_IP_2,
+  POST_IP_3,  // IP address         || Each part of an IP address has its own POST parameter.     ||
+  POST_SUBNET,
+  POST_SUBNET_1,
+  POST_SUBNET_2,
+  POST_SUBNET_3,  // subnet             || Because HTML code for IP, subnet, gateway and DNS          ||
+  POST_GATEWAY,
+  POST_GATEWAY_1,
+  POST_GATEWAY_2,
+  POST_GATEWAY_3,  // gateway            || is generated through one (nested) for-loop,                ||
+  POST_DNS,
+  POST_DNS_1,
+  POST_DNS_2,
+  POST_DNS_3,        // DNS                || all these 16 enum elements must be listed in succession!!  ||
+  POST_TCP,          // TCP port                  || Because HTML code for these 3 ports              ||
+  POST_UDP,          // UDP port                  || is generated through one for-loop,               ||
+  POST_WEB,          // web UI port               || these 3 elements must be listed in succession!!  ||
+  POST_RTU_OVER,     // RTU over TCP/UDP
+  POST_TCP_TIMEOUT,  // Modbus TCP socket close timeout
+  POST_BAUD,         // baud rate
+  POST_DATA,         // data bits
+  POST_PARITY,       // parity
+  POST_STOP,         // stop bits
+  POST_FRAMEDELAY,   //frame delay
+  POST_TIMEOUT,      // response timeout
+  POST_ATTEMPTS,     // number of request attempts
+  POST_REQ,          // Modbus request send from WebUI (first byte)
+  POST_REQ_1,
+  POST_REQ_2,
+  POST_REQ_3,
+  POST_REQ_4,
+  POST_REQ_5,
+  POST_REQ_6,
+  POST_REQ_LAST,  // 8 bytes in total
+  POST_ACTION,    // actions on Tools page
+};
+
+byte request[POST_REQ_LAST - POST_REQ + 1];  // Array to store Modbus request sent from WebUI
+byte requestLen = 0;                         // Length of the Modbus request send from WebUI
+
+
+// Keys for JSON elements, used in: 1) JSON documents, 2) ID of span tags, 3) Javascript.
+enum JSON_type : byte {
+  JSON_TIME,  // Runtime seconds
+  JSON_RTU_DATA,
+  JSON_ETH_DATA,
+  JSON_RESPONSE,
+  JSON_STATS,  // Modbus statistics from array data.errorCnt[]
+  JSON_QUEUE,
+  JSON_TCP_UDP_MASTERS,  // list of Modbus TCP/UDP masters separated by <br>
+  JSON_SLAVES,           // list of Modbus RTU slaves separated by <br>
+  JSON_SOCKETS,
+  JSON_LAST,  // Must be the very last element in this array
+};
+
+/**************************************************************************/
+/*!
+  @brief Receives GET requests for web pages, receives POST data from web forms,
+  calls @ref processPost() function, sends web pages. For simplicity, all web pages
+  should are numbered (1.htm, 2.htm, ...), the page number is passed to 
+  the @ref sendPage() function. Also executes actions (such as ethernet restart,
+  reboot) during "please wait" web page.
+  @param client Ethernet TCP client.
+*/
+/**************************************************************************/
+void recvWeb(EthernetClient &client) {
+  char uri[URI_SIZE];  // the requested page
+  memset(uri, 0, sizeof(uri));
+  while (client.available()) {        // start reading the first line which should look like: GET /uri HTTP/1.1
+    if (client.read() == ' ') break;  // find space before /uri
+  }
+  byte len = 0;
+  while (client.available() && len < sizeof(uri) - 1) {
+    char c = client.read();  // parse uri
+    if (c == ' ') break;     // find space after /uri
+    uri[len] = c;
+    len++;
+  }
+  while (client.available()) {
+    if (client.read() == '\r')
+      if (client.read() == '\n')
+        if (client.read() == '\r')
+          if (client.read() == '\n')
+            break;  // find 2 end of lines between header and body
+  }
+  if (client.available()) {
+    processPost(client);  // parse post parameters
+  }
+
+  // Get the requested page from URI
+  byte reqPage = PAGE_ERROR;  // requested page, 404 error is a default
+  if (uri[0] == '/') {
+    if (uri[1] == '\0') {
+      reqPage = PAGE_INFO;  // Homepage
+    } else if (uri[1] >= '0' && uri[1] <= '9' && strcmp(uri + 2, ".htm") == 0) {
+      reqPage = byte(uri[1] - '0');  // Convert ASCII digit to byte
+      if (reqPage > PAGE_WAIT) reqPage = PAGE_ERROR;
+    } else if (strcmp(uri, "/d.json") == 0) {
+      reqPage = PAGE_DATA;
+    }
+  }
+  // Actions that require "please wait" page
+  if (action == ACT_WEB || action == ACT_MAC || action == ACT_RESET_ETH || action == ACT_REBOOT || action == ACT_DEFAULT) {
+    reqPage = PAGE_WAIT;
+  }
+  // Send page
+  sendPage(client, reqPage);
+
+  // Do all actions before the "please wait" redirects (5s delay at the moment)
+  if (reqPage == PAGE_WAIT) {
+    delay(500);  // wait for the wait page to load
+    switch (action) {
+      case ACT_WEB:
+        for (byte s = 0; s < maxSockNum; s++) {
+          // close old webserver TCP connections
+          if (EthernetClient(s).localPort() != data.config.tcpPort) {
+            disconSocket(s);
+          }
+        }
+        webServer = EthernetServer(data.config.webPort);
+        break;
+      case ACT_MAC:
+      case ACT_RESET_ETH:
+        for (byte s = 0; s < maxSockNum; s++) {
+          // close all TCP and UDP sockets
+          disconSocket(s);
+        }
+        startEthernet();
+        break;
+      case ACT_REBOOT:
+      case ACT_DEFAULT:
+        resetFunc();
+        break;
+      default:
+        break;
+    }
+  }
+  action = ACT_NONE;
+}
+
+/**************************************************************************/
+/*!
+  @brief Processes POST data from forms and buttons, updates data.config (in RAM)
+  and saves config into EEPROM. Executes actions which do not require webserver restart
+  @param client Ethernet TCP client.
+*/
+/**************************************************************************/
+void processPost(EthernetClient &client) {
+  while (client.available()) {
+    char post[POST_SIZE];
+    byte len = 0;
+    while (client.available() && len < sizeof(post) - 1) {
+      char c = client.read();
+      if (c == '&') break;
+      post[len] = c;
+      len++;
+    }
+    post[len] = '\0';
+    char *paramKey = post;
+    char *paramValue = post;
+    while (*paramValue) {
+      if (*paramValue == '=') {
+        paramValue++;
+        break;
+      }
+      paramValue++;
+    }
+    if (*paramValue == '\0')
+      continue;  // do not process POST parameter if there is no parameter value
+    byte paramKeyByte = strToByte(paramKey);
+    uint16_t paramValueUint = atol(paramValue);
+    switch (paramKeyByte) {
+      case POST_NONE:  // reserved, because atoi / atol returns NULL in case of error
+        break;
+#ifdef ENABLE_DHCP
+      case POST_DHCP:
+        {
+          data.config.enableDhcp = byte(paramValueUint);
+        }
+        break;
+      case POST_DNS ... POST_DNS_3:
+        {
+          data.config.dns[paramKeyByte - POST_DNS] = byte(paramValueUint);
+        }
+        break;
+#endif /* ENABLE_DHCP */
+      case POST_REQ ... POST_REQ_LAST:
+        {
+          requestLen = paramKeyByte - POST_REQ + 1;
+          request[requestLen - 1] = strToByte(paramValue);
+        }
+        break;
+      case POST_MAC ... POST_MAC_5:
+        {
+          action = ACT_RESET_ETH;  // this RESET_ETH is triggered when the user changes anything on the "IP Settings" page.
+                                   // No need to trigger RESET_ETH for other cases (POST_SUBNET, POST_GATEWAY etc.)
+                                   // if "Randomize" button is pressed, action is set to ACT_MAC
+          data.mac[paramKeyByte - POST_MAC] = strToByte(paramValue);
+        }
+        break;
+      case POST_IP ... POST_IP_3:
+        {
+          data.config.ip[paramKeyByte - POST_IP] = byte(paramValueUint);
+        }
+        break;
+      case POST_SUBNET ... POST_SUBNET_3:
+        {
+          data.config.subnet[paramKeyByte - POST_SUBNET] = byte(paramValueUint);
+        }
+        break;
+      case POST_GATEWAY ... POST_GATEWAY_3:
+        {
+          data.config.gateway[paramKeyByte - POST_GATEWAY] = byte(paramValueUint);
+        }
+        break;
+      case POST_TCP:
+        {
+          if (paramValueUint != data.config.webPort && paramValueUint != data.config.tcpPort) {  // continue only of the value changed and it differs from WebUI port
+            for (byte s = 0; s < maxSockNum; s++) {
+              if (EthernetClient(s).localPort() == data.config.tcpPort) {  // close only Modbus TCP sockets
+                disconSocket(s);
+              }
+            }
+            data.config.tcpPort = paramValueUint;
+            modbusServer = EthernetServer(data.config.tcpPort);
+          }
+        }
+        break;
+      case POST_UDP:
+        {
+          data.config.udpPort = paramValueUint;
+          Udp.stop();
+          Udp.begin(data.config.udpPort);
+        }
+        break;
+      case POST_WEB:
+        {
+          if (paramValueUint != data.config.webPort && paramValueUint != data.config.tcpPort) {  // continue only of the value changed and it differs from Modbus TCP port
+            data.config.webPort = paramValueUint;
+            action = ACT_WEB;
+          }
+        }
+        break;
+      case POST_RTU_OVER:
+        data.config.enableRtuOverTcp = byte(paramValueUint);
+        break;
+      case POST_TCP_TIMEOUT:
+        data.config.tcpTimeout = paramValueUint;
+        break;
+      case POST_BAUD:
+        {
+          action = ACT_RESET_SERIAL;  // this RESET_SERIAL is triggered when the user changes anything on the "RTU Settings" page.
+          // No need to trigger RESET_ETH for other cases (POST_DATA, POST_PARITY etc.)
+          data.config.baud = paramValueUint;
+          byte minFrameDelay = byte((frameDelay() / 1000UL) + 1);
+          if (data.config.frameDelay < minFrameDelay) {
+            data.config.frameDelay = minFrameDelay;
+          }
+        }
+        break;
+      case POST_DATA:
+        {
+          data.config.serialConfig = (data.config.serialConfig & 0xF9) | ((byte(paramValueUint) - 5) << 1);
+        }
+        break;
+      case POST_PARITY:
+        {
+          data.config.serialConfig = (data.config.serialConfig & 0xCF) | (byte(paramValueUint) << 4);
+        }
+        break;
+      case POST_STOP:
+        {
+          data.config.serialConfig = (data.config.serialConfig & 0xF7) | ((byte(paramValueUint) - 1) << 3);
+        }
+        break;
+      case POST_FRAMEDELAY:
+        data.config.frameDelay = byte(paramValueUint);
+        break;
+      case POST_TIMEOUT:
+        data.config.serialTimeout = paramValueUint;
+        break;
+      case POST_ATTEMPTS:
+        data.config.serialAttempts = byte(paramValueUint);
+        break;
+      case POST_ACTION:
+        action = action_type(paramValueUint);
+        break;
+      default:
+        break;
+    }
+  }
+  switch (action) {
+    case ACT_DEFAULT:
+      data.config = DEFAULT_CONFIG;
+      break;
+    case ACT_RESET_STATS:
+      resetStats();
+      break;
+    case ACT_MAC:
+      generateMac();
+      break;
+    case ACT_RESET_SERIAL:
+      clearQueue();
+      startSerial();
+      break;
+    case ACT_SCAN:
+      scanCounter = 1;
+      memset(&slaveStatus, 0, sizeof(slaveStatus));  // clear all status flags
+      break;
+    case ACT_CLEAR_REQUEST:
+      requestLen = 0;
+      responseLen = 0;
+      break;
+    default:
+      break;
+  }
+  // if new Modbus request received, put into queue
+  if (action != ACT_SCAN && action != ACT_RESET_STATS && requestLen > 1 && queueHeaders.available() > 1 && queueData.available() > requestLen) {  // at least 2 bytes in request (slave address and function)
+    // push to queue
+    queueHeaders.push(header_t{
+      { 0x00, 0x00 },  // tid[2]
+      requestLen,      // msgLen
+      { 0, 0, 0, 0 },  // remIP[4]
+      0,               // remPort
+      UDP_REQUEST,     // requestType
+      0,               // atts
+    });
+    for (byte i = 0; i < requestLen; i++) {
+      queueData.push(request[i]);
+    }
+    responseLen = 0;  // clear old Modbus Response from WebUI
+  }
+  // new parameter values received, save them to EEPROM
+  updateEeprom();  // it is safe to call, only changed values (and changed error and data counters) are updated
+}
+
+/**************************************************************************/
+/*!
+  @brief Parses string and returns single byte.
+  @param myStr String (2 chars, 1 char + null or 1 null) to be parsed.
+  @return Parsed byte.
+*/
+/**************************************************************************/
+byte strToByte(const char myStr[]) {
+  if (!myStr) return 0;
+  byte x = 0;
+  for (byte i = 0; i < 2; i++) {
+    char c = myStr[i];
+    if (c >= '0' && c <= '9') {
+      x *= 16;
+      x += c - '0';
+    } else if (c >= 'A' && c <= 'F') {
+      x *= 16;
+      x += (c - 'A') + 10;
+    } else if (c >= 'a' && c <= 'f') {
+      x *= 16;
+      x += (c - 'a') + 10;
+    }
+  }
+  return x;
+}
+
+char __printbuffer[3];
+/**************************************************************************/
+/*!
+  @brief Converts byte to char string, from https://github.com/RobTillaart/printHelpers
+  @param val Byte to be conferted.
+  @return Char string.
+*/
+/**************************************************************************/
+char *hex(byte val) {
+  char *buffer = __printbuffer;
+  byte digits = 2;
+  buffer[digits] = '\0';
+  while (digits > 0) {
+    byte v = val & 0x0F;
+    val >>= 4;
+    digits--;
+    buffer[digits] = (v < 10) ? '0' + v : ('A' - 10) + v;
+  }
+  return buffer;
+}
